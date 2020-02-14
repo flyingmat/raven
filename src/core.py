e4105f905adbef004edba267a1bbf8bdd748ae95
flyingmat <paramattimatteo@gmail.com>
2020-02-14 22:41:31 +0000

    Threaded media downloading (images only)

--- src/core.py
+++ src/core.py
@@ -1,7 +1,8 @@
 from selenium.webdriver import Firefox, FirefoxProfile
+import threading, queue, os, urllib.request
 
 # can be increased in case of slow internet speeds and/or poor cpu performance
-IMPLICIT_WAIT = 2
+IMPLICIT_WAIT = 0.5
 
 # the Tweet class holds tweet information
 class Tweet:
@@ -9,10 +10,25 @@ class Tweet:
         self.text = text
         self.media_urls = media_urls
 
+class MediaDowloadThread(threading.Thread):
+    def __init__(self, queue, overwrite=False):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.overwrite = overwrite
+
+    def run(self):
+        while True:
+            if media_url := self.queue.get():
+                download_media(media_url, self.overwrite)
+            else:
+                break
+            self.queue.task_done()
+
 # return a tweet's text given its webelement
 def tweet_text(tweet_element):
     return tweet_element.find_element_by_xpath(".//p[contains(@class, 'tweet-text')]").text
 
+# return a list of a tweet's image urls
 def tweet_media_urls(tweet_element):
     try:
         media_container = tweet_element.find_element_by_xpath(".//div[@class='AdaptiveMediaOuterContainer']")
@@ -21,6 +37,17 @@ def tweet_media_urls(tweet_element):
         return media_urls
     except:
         return []
+
+def download_media(media_url, overwrite=False):
+    filename = media_url.split('/')[-1].split(':')[0]
+    if not os.path.exists(filename) or overwrite:
+        print('    - Downloading {}...'.format(filename))
+        with urllib.request.urlopen(media_url, timeout=5) as media:
+            media_data = media.read()
+        with open(filename, 'wb') as media_file:
+            media_file.write(media_data)
+    elif not overwrite:
+        print('    - File {} already exists!'.format(filename))
 
 # tweet scraper, implemented as generator
 def profile_tweet_scrape(driver):
@@ -40,11 +67,22 @@ def profile_tweet_scrape(driver):
         return
 
 # profile dumping function, prints all of a user's tweets
-def profile_dump(driver, profile_url):
+def profile_dump(driver, profile_url, download_media=True, overwrite_media=False):
     driver.get(profile_url)
 
+    if download_media:
+        download_queue = queue.Queue()
+        download_thread = MediaDowloadThread(download_queue, overwrite=overwrite_media)
+        download_thread.start()
     try:
         for tweet in profile_tweet_scrape(driver):
-            print(tweet.text)
-    except:
+            if download_media:
+                for media_url in tweet.media_urls:
+                    download_queue.put(media_url)
+    except Exception as e:
         raise
+    finally:
+        print('(!) Core halting...')
+        download_queue.join()
+        download_queue.put(None)
+        download_thread.join()
