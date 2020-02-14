065e24dd6791c1842402b47d8d09bcdd2c4e75fd
flyingmat <paramattimatteo@gmail.com>
2020-02-14 02:16:59 +0000

    Comments and fixes

--- src/core.py
+++ src/core.py
@@ -1,12 +1,15 @@
 from selenium.webdriver import Firefox, FirefoxProfile
 
+# the Tweet class holds tweet information
 class Tweet:
     def __init__(self, text, media = []):
         self.text = text
 
+# return a tweet's text given its webelement
 def tweet_text(tweet_element):
     return tweet_element.find_element_by_xpath(".//p[contains(@class, 'tweet-text')]").text
 
+# tweet scraper, implemented as generator
 def profile_tweet_scrape(driver):
     tweet_list = driver.find_element_by_xpath("//div[@id='timeline']")
     tweet = tweet_list.find_element_by_xpath(".//li[contains(@id, 'stream-item-tweet')]")
@@ -21,12 +24,12 @@ def profile_tweet_scrape(driver):
     except:
         raise StopIteration
 
+# profile dumping function, prints all of a user's tweets
 def profile_dump(driver, profile_url):
     driver.get(profile_url)
 
     try:
         for tweet in profile_tweet_scrape(driver):
             print(tweet.text)
-    except:
-        return False
-    return True
+    except StopIteration:
+        pass
--- src/raven.py
+++ src/raven.py
@@ -1,14 +1,18 @@
 from core import *
 import argparse
 
+# cli argument parser
 parser = argparse.ArgumentParser(description='Raven is a Twitter scraping utility written in python.')
 parser.add_argument('profile', metavar='PROFILE', help='Profile URL')
 
+# creates a properly setup webdriver instance
 def init_driver():
     ffprofile = FirefoxProfile()
+    # fix to get the old twitter ui
     ffprofile.set_preference('general.useragent.override', 'Mozilla/5.0 (Windows NT 9.0; WOW64; Trident/7.0; rv:11.0) like Gecko')
 
     driver = Firefox(ffprofile)
+    # can be increased in case of slow internet speeds and/or poor cpu performance
     driver.implicitly_wait(5)
     driver.set_window_size(1000,1000)
 
@@ -18,9 +22,12 @@ def main():
     args = parser.parse_args()
 
     driver = init_driver()
-    profile_dump(driver, args.profile)
-
-    driver.quit()
+    try:
+        profile_dump(driver, args.profile)
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
 
 if __name__ == '__main__':
     main()
